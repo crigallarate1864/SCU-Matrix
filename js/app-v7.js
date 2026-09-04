fe36e89665178787bc2437cfa53c64db16b28772
crigallarate1864 <adriano.luongo@crigallarate.org>
2026-09-04 11:57:57 +0200

    Load SCU monthly email generator

--- js/app-v7.js
+++ js/app-v7.js
@@ -1,4 +1,5 @@
-import './app-v2.js?v=0.7.0';
-import './progress-v1.js?v=0.7.0';
-import './hotfix-v2.js?v=0.7.0';
-import './dashboard-v1.js?v=0.7.0';
+import './app-v2.js?v=0.8.0';
+import './progress-v1.js?v=0.8.0';
+import './hotfix-v2.js?v=0.8.0';
+import './dashboard-v1.js?v=0.8.0';
+import './mail-v1.js?v=0.8.0';
